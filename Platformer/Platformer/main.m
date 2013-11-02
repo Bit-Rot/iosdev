88ae3bee0168706f19be9e2fab29751252da6783
BitRot <bitrot@BitRots-Mac.local>
2013-11-02 14:00:45 -0700

    initial platformer commit

---
+++ Platformer/Platformer/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Platformer
+//
+//  Created by BitRot on 11/2/13.
+//  Copyright BitRot 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
